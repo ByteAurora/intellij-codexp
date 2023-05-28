0b2a0e4c8d89c81ffb2e71d2b8136b35ec29c14f
GitHub Action <action@github.com>
2023-05-28 06:37:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.ilovegamecoding.intellijcodexp.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
