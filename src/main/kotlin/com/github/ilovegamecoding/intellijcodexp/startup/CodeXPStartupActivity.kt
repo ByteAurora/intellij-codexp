21a75ea2a3d1342e9fb9b21dda12c5f728d5ceff
Sohn Young Jin <sonkim1001@naver.com>
2023-05-31 01:59:44 +0900

    feat: Add initialize plugin logic from CodeXPService and add typing, action challenge

--- src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/startup/CodeXPStartupActivity.kt
+++ src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/startup/CodeXPStartupActivity.kt
@@ -1,18 +1,39 @@
 package com.github.ilovegamecoding.intellijcodexp.startup
 
-import com.github.ilovegamecoding.intellijcodexp.core.CodeXP
+import com.github.ilovegamecoding.intellijcodexp.model.CodeXPChallenge
+import com.github.ilovegamecoding.intellijcodexp.services.CodeXPService
 import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
+/**
+ * CodeXPStartupActivity class
+ *
+ * This class is used to initialize the plugin when the IDE starts.
+ */
 class CodeXPStartupActivity : StartupActivity {
     override fun runActivity(project: Project) {
-        val state = service<CodeXP>()
-        if (!state.hasExecuted) {
-            // TODO: Add plugin initialize logic
-
-            state.hasExecuted = true
+        with(service<CodeXPService>()) {
+            initializePlugin(
+                listOf(
+                    CodeXPChallenge(
+                        type = CodeXPChallenge.Type.TYPING_COUNT.ordinal,
+                        name = "Typing",
+                        description = "Just type anything. We will give you XP for it.",
+                        rewardXP = 100,
+                        value = 0,
+                        goal = 10
+                    ),
+                    CodeXPChallenge(
+                        type = CodeXPChallenge.Type.ACTION_COUNT.ordinal,
+                        name = "Action",
+                        description = "Just do anything. We will give you XP for it.",
+                        rewardXP = 100,
+                        value = 0,
+                        goal = 10
+                    )
+                )
+            )
         }
     }
 }
