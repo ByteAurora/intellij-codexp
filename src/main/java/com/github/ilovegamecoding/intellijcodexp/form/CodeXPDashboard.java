b8bdc490dd1ef3ec9f446281845e2169b61f4aed
Sohn Young Jin <sonkim1001@naver.com>
2023-06-01 19:18:12 +0900

    feat: Create CodeXP tool window ui form with gui designer

---
+++ src/main/java/com/github/ilovegamecoding/intellijcodexp/form/CodeXPDashboard.java
@@ -0,0 +1,18 @@
+package com.github.ilovegamecoding.intellijcodexp.form;
+
+import javax.swing.*;
+
+public class CodeXPDashboard {
+    public JPanel pMain;
+    public JProgressBar pbCurrentLevelProgress;
+    public JLabel lblNextLabel;
+    public JLabel lblCurrentLevel;
+    public JLabel tvCurrentLevelXP;
+    public JTextField nicknameTextField;
+    public JPanel clChallenges;
+    public JPanel clCompletedChallenges;
+    public JPanel lblCompletedChallenges;
+    public JLabel lblTotalXP;
+    public JLabel lblLevel;
+    public JButton resetButton;
+}
