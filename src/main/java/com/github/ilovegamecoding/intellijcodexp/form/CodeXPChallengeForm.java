5ae7ff49fe689889add635d249cd4137f99a41a0
Sohn Young Jin <sonkim1001@naver.com>
2023-06-04 02:15:34 +0900

    feat: Create ui form for CodeXP challenge item

---
+++ src/main/java/com/github/ilovegamecoding/intellijcodexp/form/CodeXPChallengeForm.java
@@ -0,0 +1,14 @@
+package com.github.ilovegamecoding.intellijcodexp.form;
+
+import javax.swing.*;
+
+public class CodeXPChallengeForm {
+    public String challengeID;
+    public JProgressBar pbChallengeProgress;
+    public JLabel lblChallengeDescription;
+    public JLabel lblChallengeName;
+    public JLabel lblChallengeProgress;
+    public JLabel lblChallengeReward;
+    public JPanel pChallenge;
+    public JLabel lblChallengePercentageIcon;
+}
