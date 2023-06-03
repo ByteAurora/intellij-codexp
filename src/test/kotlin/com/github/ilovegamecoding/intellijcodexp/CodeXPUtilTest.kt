daaa7154f61e0a1e767e8f091e8a317658947e09
Sohn Young Jin <sonkim1001@naver.com>
2023-06-03 14:18:28 +0900

    test: Add test logic for StringUtil

---
+++ src/test/kotlin/com/github/ilovegamecoding/intellijcodexp/CodeXPUtilTest.kt
@@ -0,0 +1,17 @@
+package com.github.ilovegamecoding.intellijcodexp
+
+import com.intellij.testFramework.TestDataPath
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+@TestDataPath("\$CONTENT_ROOT/src/test/testData")
+class CodeXPUtilTest : BasePlatformTestCase() {
+    fun testStringUtil() {
+        val number1: Long = 123456789
+        val number2: Long = 0
+        val number3: Long = 12345
+
+        assertEquals("123,456,789", StringUtil.numberToStringWithCommas(number1))
+        assertEquals("0", StringUtil.numberToStringWithCommas(number2))
+        assertEquals("12,345", StringUtil.numberToStringWithCommas(number3))
+    }
+}
