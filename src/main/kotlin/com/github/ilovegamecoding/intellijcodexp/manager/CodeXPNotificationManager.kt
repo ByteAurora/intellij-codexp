9b69c565f4e3f7244d58f584c86eddae97d3d309
Sohn Young Jin <sonkim1001@naver.com>
2023-06-04 14:11:07 +0900

    feat: Add NotificationManager for CodeXP notifications

---
+++ src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/manager/CodeXPNotificationManager.kt
@@ -0,0 +1,23 @@
+package com.github.ilovegamecoding.intellijcodexp.manager
+
+import com.github.ilovegamecoding.intellijcodexp.model.CodeXPChallenge
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+
+object CodeXPNotificationManager {
+    private val notifiationGroup: NotificationGroup =
+        NotificationGroupManager.getInstance().getNotificationGroup("CodeXP")
+
+    fun notify(title: String, content: String) {
+        notifiationGroup.createNotification(title, content, NotificationType.INFORMATION).notify(null)
+    }
+
+    fun notifyChallengeComplete(codeXPChallenge: CodeXPChallenge) {
+        notify("${codeXPChallenge.name} completed!", "Reward: ${codeXPChallenge.rewardXP} XP")
+    }
+
+    fun notifyLevelUp(level: Int, xpToNextLevel: Long) {
+        notify("Level up!", "Congratulations! You are now level $level. You need $xpToNextLevel XP to reach the next level.")
+    }
+}
