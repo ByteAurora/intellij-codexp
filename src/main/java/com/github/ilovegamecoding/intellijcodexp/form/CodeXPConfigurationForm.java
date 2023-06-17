e155f812916206c17565b8beac9fdd3a5e9a6a59
Sohn Young Jin <sonkim1001@naver.com>
2023-06-18 03:06:21 +0900

    feat: Create UI form for CodeXP configuration

---
+++ src/main/java/com/github/ilovegamecoding/intellijcodexp/form/CodeXPConfigurationForm.java
@@ -0,0 +1,10 @@
+package com.github.ilovegamecoding.intellijcodexp.form;
+
+import javax.swing.*;
+
+public class CodeXPConfigurationForm {
+    public JCheckBox cbShowCompleteChallengeNotification;
+    public JCheckBox cbShowLevelUpNotification;
+    public JCheckBox cbShowCompletedChallenges;
+    public JPanel pMain;
+}
