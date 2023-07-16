5566daf0312b9f5367f06a45cdee5811f7b57bd6
Sohn Young Jin <sonkim1001@naver.com>
2023-07-17 01:02:42 +0900

    feat: Add conditional statements for the 'Enter' event and implement the display of experience point gain

--- src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/listeners/CodeXPEventListener.kt
+++ src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/listeners/CodeXPEventListener.kt
@@ -1,13 +1,16 @@
 package com.github.ilovegamecoding.intellijcodexp.listeners
 
 import com.github.ilovegamecoding.intellijcodexp.services.CodeXPService
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.AnActionResult
-import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.ui.JBColor
+import java.awt.Color
+import java.awt.Font
+import javax.swing.JComponent
+import javax.swing.JLabel
+import kotlin.math.max
 
 /**
  * CodeXPEventListener class
@@ -15,9 +18,20 @@ import com.intellij.openapi.diagnostic.thisLogger
  * This class listens to events from the IDE and fires them to the message bus.
  */
 internal class CodeXPEventListener : AnActionListener {
+    /**
+     * Fading labels in each swing component.
+     */
+    private val fadingLabels: MutableMap<JComponent, FadingLabel> = mutableMapOf()
+
+    /**
+     * Current value of the fading label.
+     */
+    private var currentXPGainValue: Int = 0
+
     override fun afterEditorTyping(c: Char, dataContext: DataContext) {
         super.afterEditorTyping(c, dataContext)
-        fireEvent(CodeXPService.Event.TYPING)
+
+        fireEventAndDisplayXPLabel(CodeXPService.Event.TYPING, dataContext)
     }
 
     override fun afterActionPerformed(action: AnAction, event: AnActionEvent, result: AnActionResult) {
@@ -30,11 +44,23 @@ internal class CodeXPEventListener : AnActionListener {
             "Debug" -> fireEvent(CodeXPService.Event.DEBUG)
             "Build Project" -> fireEvent(CodeXPService.Event.BUILD)
             "Rebuild Project" -> fireEvent(CodeXPService.Event.BUILD)
-            "Paste" -> fireEvent(CodeXPService.Event.PASTE)
-            "Backspace" -> fireEvent(CodeXPService.Event.BACKSPACE)
-            "Tab" -> fireEvent(CodeXPService.Event.TAB)
+            "Paste" -> fireEventAndDisplayXPLabel(CodeXPService.Event.PASTE, event.dataContext)
+            "Backspace" -> fireEventAndDisplayXPLabel(CodeXPService.Event.BACKSPACE, event.dataContext)
+            "Tab" -> fireEventAndDisplayXPLabel(CodeXPService.Event.TAB, event.dataContext)
+            "Enter" -> fireEventAndDisplayXPLabel(CodeXPService.Event.ENTER, event.dataContext)
             else -> fireEvent(CodeXPService.Event.ACTION)
         }
+    }
+
+    /**
+     * Fire event to the message bus and display XP label.
+     *
+     * @param event The event to fire.
+     * @param dataContext The data context of the event.
+     */
+    private fun fireEventAndDisplayXPLabel(event: CodeXPService.Event, dataContext: DataContext) {
+        fireEvent(event)
+        displayXPLabel(event, dataContext)
     }
 
     /**
@@ -45,5 +71,86 @@ internal class CodeXPEventListener : AnActionListener {
     private fun fireEvent(event: CodeXPService.Event) {
         ApplicationManager.getApplication().messageBus.syncPublisher(CodeXPListener.CODEXP_EVENT)
             .eventOccurred(event)
+    }
+
+    /**
+     * Display XP label at the caret position.
+     *
+     * @param event The event to fire.
+     * @param dataContext The data context of the event.
+     */
+    private fun displayXPLabel(event: CodeXPService.Event, dataContext: DataContext) {
+        val editor = CommonDataKeys.EDITOR.getData(dataContext) ?: return
+        val caretModel = editor.caretModel
+        val point = editor.visualPositionToXY(caretModel.visualPosition)
+
+        val component = editor.contentComponent
+
+        fadingLabels[component]?.let { fadingLabel ->
+            fadingLabel.cancelFadeOut()
+            currentXPGainValue = fadingLabel.value
+            component.remove(fadingLabel)
+            component.revalidate()
+            component.repaint()
+        }
+
+        currentXPGainValue += event.xpValue.toInt()
+
+        val newFadingLabel = FadingLabel(currentXPGainValue).apply {
+            font = Font(font.fontName, Font.BOLD, 14)
+            size = preferredSize
+            point.let { p ->
+                p.translate(4, -(height / 2))
+                location = p
+            }
+            startFadeOut()
+        }
+
+        fadingLabels[component] = newFadingLabel
+
+        component.add(newFadingLabel)
+        component.revalidate()
+        component.repaint()
+    }
+
+    internal class FadingLabel(initialValue: Int) : JLabel(if (initialValue == 0) "0 xp" else "+$initialValue XP") {
+        private lateinit var timer: javax.swing.Timer
+
+        /**
+         * Start fade out animation.
+         */
+        fun startFadeOut() {
+            timer = javax.swing.Timer(100, null)
+            timer.addActionListener {
+                val newAlpha = max(foreground.alpha - 255 / 10, 0)
+                if (newAlpha <= 0) {
+                    timer.stop()
+                    value = 0
+                } else {
+                    foreground =
+                        Color(JBColor.foreground().red, JBColor.foreground().green, JBColor.foreground().blue, newAlpha)
+                }
+            }
+            timer.start()
+        }
+
+        /**
+         * Cancel fade out animation.
+         */
+        fun cancelFadeOut() {
+            timer.stop()
+        }
+
+        /**
+         * Current value of the fading label.
+         */
+        var value: Int = initialValue
+            set(newValue) {
+                field = newValue
+                text = if (newValue == 0) "0 xp" else "+$newValue xp"
+                if (newValue == 0) {
+                    timer.stop()
+                }
+            }
     }
 }
