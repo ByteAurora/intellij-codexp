462e365a31f73c031c95a1add90423c3dbdf9813
Sohn Young Jin <sonkim1001@naver.com>
2023-08-28 12:39:59 +0900

    feat: Remove print log

--- src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/activities/CodeXPStartupActivity.kt
+++ src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/activities/CodeXPStartupActivity.kt
@@ -2,13 +2,11 @@ package com.github.ilovegamecoding.intellijcodexp.activities
 
 import com.github.ilovegamecoding.intellijcodexp.services.CodeXPService
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 
 class CodeXPStartupActivity : StartupActivity {
     override fun runActivity(project: Project) {
-        thisLogger().warn("CodeXPStartupActivity.runActivity")
         val codeXPService = ApplicationManager.getApplication().getService(CodeXPService::class.java)
     }
 }
