4268b34d678871296b36e6b9916f0e6fa8c34b20
Sohn Young Jin <sonkim1001@naver.com>
2023-08-28 23:07:15 +0900

    feat: Add CodeXPListener class for xp and challenge update

--- src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/listeners/CodeXPListener.kt
+++ src/main/kotlin/com/github/ilovegamecoding/intellijcodexp/listeners/CodeXPListener.kt
@@ -1,27 +1,29 @@
 package com.github.ilovegamecoding.intellijcodexp.listeners
 
 import com.github.ilovegamecoding.intellijcodexp.enums.Event
-import com.intellij.openapi.actionSystem.DataContext
+import com.github.ilovegamecoding.intellijcodexp.models.CodeXPChallenge
+import com.github.ilovegamecoding.intellijcodexp.models.CodeXPLevel
 import com.intellij.util.messages.Topic
 
-/**
- * CodeXPListener interface
- *
- * This interface is used to listen to events from the CodeXP plugin.
- */
 interface CodeXPListener {
     companion object {
         /**
          * Topic for CodeXP events.
          */
-        val CODEXP_EVENT: Topic<CodeXPListener> = Topic.create("CodeXP Event", CodeXPListener::class.java)
+        val CODEXP: Topic<CodeXPListener> = Topic.create("CodeXP", CodeXPListener::class.java)
     }
 
     /**
-     * Function that is called when an event occurs.
+     * Function that is called when level up.
      *
-     * @param event The event that occurred.
-     * @param dataContext The data context of the event.
+     * @param levelInfo The new level.
      */
-    fun eventOccurred(event: Event, dataContext: DataContext? = null)
+    fun xpUpdated(levelInfo: CodeXPLevel)
+
+    /**
+     * Function that is called when challenge completed.
+     *
+     * @param challenge The completed challenge.
+     */
+    fun challengeUpdated(event: Event, challenge: CodeXPChallenge, newChallenge: CodeXPChallenge? = null)
 }
